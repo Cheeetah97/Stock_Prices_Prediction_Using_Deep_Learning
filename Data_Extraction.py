9a7175c945b304f639de75c5c82314c955769706
Ali <sa22007@essex.ac.uk>
2023-05-20 12:42:34 +0100

    Update

--- Data_Extraction.py
+++ Data_Extraction.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import yfinance as yf
 from datetime import datetime
-import matplotlib.pyplot as plt
-import pytz
+
+data_path = "M:/Dissertation/Data/"
 
 #%%
 # Functions 
@@ -22,48 +22,62 @@ def convert_to_utc(local_time,time_format):
     return utc_time
 
 #%%
-# List all the Stocks
+# Extracting Stock Data and converting Timezones and Currencies where necessary
 
 stocks = {"Shell_PLC":"SHEL.L",
           "Total_Energies_SE":"TTE.L",
           "Bp_PLC":"BP.L",
-          "Woodside_Energy":"WDS.L",
+          "Glencore_PLC":"GLEN.L",
           "Harbour_Energy":"HBR.L",
           "Energean_PLC":"ENOG.L",
           "Thungela_Resources":"TGA.L",
           "ITM_Power":"ITM.L",
-          "Ceres_Power":"CWR.L"}
+          "Ceres_Power":"CWR.L",
+          "Tullow_Oil_PLC":"TLW.L",
+          "Diversified_Energy":"DEC.L",
+          "Serica_Energy":"SQZ.L"}
 
 currencies = {"Shell_PLC":"GBp",
               "Total_Energies_SE":"EUR",
               "Bp_PLC":"GBp",
-              "Woodside_Energy":"GBp",
+              "Glencore_PLC":"GBp",
               "Harbour_Energy":"GBp",
               "Energean_PLC":"GBp",
               "Thungela_Resources":"GBp",
               "ITM_Power":"GBp",
-              "Ceres_Power":"GBp"}
+              "Ceres_Power":"GBp",
+              "Tullow_Oil_PLC":"GBp",
+              "Diversified_Energy":"GBp",
+              "Serica_Energy":"GBp"}
 
 
-start_date = "2021-07-31"#datetime.today() - pd.DateOffset(days=729)
-end_date = datetime.today()
+start_date = "2022-01-31"
+end_date = datetime.today() - pd.DateOffset(days=1)
 time_format = "%Y-%m-%d %H:%M:%S%z"
 
 stocks_df = pd.DataFrame()
 for stk in stocks.items():
-    stock_data = extract_data(stk[1],start_date,end_date).reset_index()[["Datetime","Open"]]
+    stock_data = extract_data(stk[1],start_date,end_date).reset_index()[["Datetime","Open","High","Low","Close"]]
     stock_data["Datetime"] = stock_data["Datetime"].apply(lambda x:convert_to_utc(x,time_format))
     if currencies[stk[0]] != "GBp":
-        currency_data = extract_data(str(currencies[stk[0]])+"GBP=X",start_date,end_date).reset_index()[["Datetime","Open"]].rename(columns={"Open":str(currencies[stk[0]])+"GBP=X"})
+        currency_data = extract_data(str(currencies[stk[0]])+"GBP=X",start_date,end_date)
+        currency_data = currency_data.reset_index()[["Datetime","Open","High","Low","Close"]]
+        currency_data = currency_data.rename(columns={"Open":"Open"+str(currencies[stk[0]])+"GBP=X",
+                                                      "High":"High"+str(currencies[stk[0]])+"GBP=X",
+                                                      "Low":"Low"+str(currencies[stk[0]])+"GBP=X",
+                                                      "Close":"Close"+str(currencies[stk[0]])+"GBP=X"})
         currency_data["Datetime"] = currency_data["Datetime"].apply(lambda x:convert_to_utc(x,time_format))
         stock_data = stock_data.merge(currency_data,how="left",on=["Datetime"])
-        stock_data["Open"] = stock_data["Open"] * stock_data[str(currencies[stk[0]])+"GBP=X"] * 100
-        stock_data = stock_data[["Datetime","Open"]]
+        stock_data["Open"] = stock_data["Open"] * stock_data["Open"+str(currencies[stk[0]])+"GBP=X"] * 100
+        stock_data["High"] = stock_data["High"] * stock_data["High"+str(currencies[stk[0]])+"GBP=X"] * 100
+        stock_data["Low"] = stock_data["Low"] * stock_data["Low"+str(currencies[stk[0]])+"GBP=X"] * 100
+        stock_data["Close"] = stock_data["Close"] * stock_data["Close"+str(currencies[stk[0]])+"GBP=X"] * 100
+        stock_data = stock_data[["Datetime","Open","High","Low","Close"]]
     stock_data["Stock"] = stk[1]
     stocks_df = pd.concat([stocks_df,stock_data])
     
 print(stocks_df["Stock"].value_counts())
-        
+#stocks_df.to_csv(data_path+"Stocks_Data_UnPivoted.csv",index=False)
         
 
 
--- Preprocessing_and_Return_Calculation.py
+++ Preprocessing_and_Return_Calculation.py
@@ -1,13 +1,55 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-path = "F:/Masters/Dissertation/"
+data_path = "M:/Dissertation/Data/"
+#%%
+# Functions
 
-data = pd.read_csv(path+"Sample_Data.csv")
+def calculate_bollinger_bands(data,window_size=9*7,num_std=5):
+    rolling_mean = data.iloc[:-1].rolling(window=window_size).mean()
+    rolling_std = data.iloc[:-1].rolling(window=window_size).std()
+    upper_band = rolling_mean + (rolling_std * num_std)
+    lower_band = rolling_mean - (rolling_std * num_std)
+    return upper_band,lower_band,rolling_mean
+#%%
+# Outlier Removal
 
-data["Date"] = pd.to_datetime(data["Date"],format="%d/%m/%Y")
-data = data[["Date","SHEL.L"]]
+stock_data = pd.read_csv(data_path+"Stocks_Data_UnPivoted.csv")
+stock_data["Datetime"] = pd.to_datetime(stock_data["Datetime"],format="%Y-%m-%d %H:00:00")
 
-data["Price_Return"] = (data["SHEL.L"].shift(1)/data["SHEL.L"])-1
+stocks = []
+low_counts = []
+high_counts = []
+df = pd.DataFrame()
+for stk in list(stock_data.Stock.unique())[:1]:
+    temp = stock_data[stock_data.Stock==stk]
+    temp = temp.sort_values(by=["Datetime"])
+    temp = temp.reset_index(drop=True)
+    
+    for col in ["Open","High","Low","Close"]:
+        temp["Upper_Band"],temp["Lower_Band"],temp["Rolling_Mean"] = calculate_bollinger_bands(temp[col])
+        temp.loc[temp.Upper_Band.isnull(),"Upper_Band"] = temp.loc[temp.Upper_Band.isnull(),col]
+        temp.loc[temp.Lower_Band.isnull(),"Lower_Band"] = temp.loc[temp.Lower_Band.isnull(),col]
+        
+        stocks.append(stk+"_"+col)
+        high_counts.append(len(temp.loc[temp[col]>temp.Upper_Band,col]))
+        low_counts.append(len(temp.loc[temp[col]<temp.Lower_Band,col]))
+        
+        temp.loc[temp[col]>temp.Upper_Band,col] = temp["Rolling_Mean"]
+        temp.loc[temp[col]<temp.Lower_Band,col] = temp["Rolling_Mean"]
+    
+    temp = temp.drop(["Lower_Band","Upper_Band","Rolling_Mean"],axis=1)
+    df = pd.concat([df,temp])
 
-plt.plot(data["Price_Return"])
+stock_data = df.reset_index(drop=True).copy()
+corrections_df = pd.DataFrame(data={"Stock":stocks,"Low_Counts":low_counts,"High_Counts":high_counts})
+#%%
+# Price Return Calculation
+
+
+#%%
+stock_data = pd.pivot_table(stock_data,values=["Open","High","Low","Close"],index="Datetime",columns="Stock")
+stock_data = stock_data.reset_index()
+stock_data.columns = ['_'.join(col).strip() if col[0] != "Datetime" else ' '.join(col).strip() for col in stock_data.columns.values]
+stock_data.to_csv(data_path+"Stocks_Data.csv",index=False)
