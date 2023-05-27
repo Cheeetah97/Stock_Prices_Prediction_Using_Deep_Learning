8c86eef15f50fec2cb3bb7cb7e41d56c7d2226f5
Shariq Ali <mshariq.ali97@gmail.com>
2023-05-27 22:36:50 +0100

    next n sum modelling

--- Modelling.py
+++ Modelling.py
@@ -1,45 +1,18 @@
+#%%
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 data_path = "M:/Dissertation/Data/"
+#%%
 
+stock_data = pd.read_csv(data_path+"Stocks_Data.csv")
+stock_data["Datetime"] = pd.to_datetime(stock_data["Datetime"],format="%Y-%m-%d %H:00:00")
 
-# stock_data = pd.read_csv(data_path+"Stocks_Data.csv")
-# stock_data = stock_data.loc[stock_data.Stock=="TGA.L"]
-# stock_data["Datetime"] = pd.to_datetime(stock_data["Datetime"],format="%Y-%m-%d %H:00:00")
-
+stock_data = stock_data.loc[stock_data.Stock=="BP.L"].reset_index(drop=True)
+stock_data = stock_data.sort_values(by=["Datetime"])
+stock_data = stock_data.drop("Stock",axis=1)
 
 # pos = stock_data.loc[stock_data.C>0,"C"]
 # neg = stock_data.loc[stock_data.C<0,"C"]
-
-#%%
-data = pd.read_csv(data_path+"Stocks_Data(Prices).csv")
-data = data.loc[data.Stock=="BP.L"]
-data = data[["Datetime","Open","Stock"]]
-data = data.reset_index(drop=True)
-
-data["Datetime"] = pd.to_datetime(data["Datetime"],format="%Y-%m-%d %H:00:00")
-data["Day"] = data["Datetime"].dt.day
-data["Hour"] = data["Datetime"].dt.hour
-data["Month"] = data["Datetime"].dt.month
-
-#data["Diff_1"] = data["Open"].shift(1) - data["Open"]
-#data = data.dropna(subset=["Diff_1"])
-#data["Diff_1"] = np.abs(data["Diff_1"])
-
-for stk in list(data.Stock.unique()):
-    temp = data[data.Stock==stk]
-    temp = temp.sort_values(by=["Datetime"])
-    temp = temp.reset_index(drop=True)
-    
-    for i in range(len(temp)-1,2,-1):
-        if (temp["Hour"].iloc[i] == 8)
-        thr = 0.10*np.mean(temp["Target"].iloc[:i])
-        difa = temp["Target"].iloc[i]-temp["Target"].iloc[i-1]
-        if (difa >= thr) or (difa <= -thr):  
-            if difa > 0:
-                temp["Target"].iloc[:i] += difa - 0.0045 
-            else:
-                temp["Target"].iloc[:i] += difa + 0.0043
-
-#data2 = data.groupby(by=["Hour"],as_index=False)["Diff_1"].median()
+# %%
--- Preprocessing_and_Return_Calculation.py
+++ Preprocessing_and_Return_Calculation.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,8 +14,19 @@ def calculate_bollinger_bands(series,window_size=9*7,num_std=5):
     lower_band = rolling_mean - (rolling_std * num_std)
     return upper_band,lower_band,rolling_mean
 
-def calculate_price_return(series):
+def calculate_return(series):
     return (series.shift(-1)/series)-1
+
+def calculate_nfuture_sum(sum_horizon,series):
+    p_sum = []
+    for n in range(1,len(series)+1,1):
+        curr = series.iloc[[n-1]]
+        next = series.iloc[n:n+sum_horizon]
+        if len(next)==sum_horizon:
+            p_sum.append(next.sum())
+        else:
+            p_sum.append(np.nan)
+    return pd.Series(p_sum)
 #%%
 # Outlier Removal and Price Return Calculation
 
@@ -71,25 +83,33 @@ for stk in list(stock_data.Stock.unique()):
             if (temp["Day_Diff"].iloc[i]<-1)&(temp["Hour"].iloc[i] == 8):
                 prev = temp[["Hour","Day_Diff",col]].iloc[:i]
                 thr = 0.020*np.mean(prev.loc[~((prev.Hour.isin([8,9]))&(prev.Day_Diff!=-1)),col])
-                difa = temp[col].iloc[i]-temp[col].iloc[i-1]
-                if (difa >= thr) or (difa <= -thr):
-                    if difa > 0:
-                        temp[col].iloc[:i] += difa - (thr/2)
+                dif = temp[col].iloc[i]-temp[col].iloc[i-1]
+                if (dif >= thr) or (dif <= -thr):
+                    if dif > 0:
+                        temp[col].iloc[:i] += dif - (thr/2)
                     else:
-                        temp[col].iloc[:i] += difa + (thr/2)
+                        temp[col].iloc[:i] += dif + (thr/2)
+
+        # Removing the Effect of Missing Hours of a Day
+        temp[col+"_Sum"] = calculate_nfuture_sum(8,temp[col])
     
         # Calculating Price Return
-        temp[col[0]] = calculate_price_return(temp[col])
+        temp[col[0]] = calculate_return(temp[col+"_Sum"])
     
     df = pd.concat([df,temp])
 
-df[["Datetime","Open","High","Low","Close","Stock"]].to_csv(data_path+"Stocks_Data(Prices).csv",index=False)
-df[["Datetime","O","H","L","C","Stock"]].to_csv(data_path+"Stocks_Data.csv",index=False)
-
-stock_data = df[["Datetime","O","H","L","C","Stock"]].reset_index(drop=True).copy()
 corrections_df = pd.DataFrame(data={"Stock":stocks,"Low_Counts":low_counts,"High_Counts":high_counts})
+
+df[["Datetime","Open","High","Low","Close","Stock",
+    "Open_Sum","High_Sum","Low_Sum","Close_Sum"]].to_csv(data_path+"Stocks_Data(Prices).csv",index=False)
+
+stock_data = df[["Datetime","Open","High","Low","Close","O","H","L","C","Stock"]].reset_index(drop=True).copy()
+stock_data = stock_data[stock_data.groupby(by=["Stock"]).cumcount(ascending=False) > 0]
+stock_data.to_csv(data_path+"Stocks_Data.csv",index=False)
 #%%
-stock_data = pd.pivot_table(stock_data,values=["O","H","L","C"],index="Datetime",columns="Stock")
+# Pivoting the Data and Saving
+stock_data = pd.pivot_table(stock_data,values=["Open","High","Low","Close","O","H","L","C"],index="Datetime",columns="Stock")
 stock_data = stock_data.reset_index()
 stock_data.columns = ['_'.join(col).strip() if col[0] != "Datetime" else ' '.join(col).strip() for col in stock_data.columns.values]
 stock_data.to_csv(data_path+"Stocks_Data_Pivoted.csv",index=False)
+# %%
